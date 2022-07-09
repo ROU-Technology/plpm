a3b1b935aeafd7891db7d6d14c314782fd2ee867
Lebohang Bernard <lbmotseki@gmail.com>
2022-07-09 19:53:01 +0200

    Sub-Headline (a church that's relevant).

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ChurchRelevantComponent } from './church-relevant/church-relevant.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChurchRelevantComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/church-relevant/church-relevant.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'plpm-church-relevant',
+  templateUrl: './church-relevant.component.html',
+  styleUrls: ['./church-relevant.component.scss']
+})
+export class ChurchRelevantComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
