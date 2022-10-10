7865fd48dc15e437c51aaa9601ffd33d1bdbde79
SOG-web <raheemolalekanusman84@gmail.com>
2022-10-10 19:42:12 +0100

    working on routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { SermonService } from './shared/sermon.service';
 import { SermonModule } from './sermon/sermon.module';
 import { EventDetailsModule } from './event-details/event-details.module';
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ContactUsModule } from './contact-us/contact-us.module';
@@ -15,23 +14,17 @@ import { EventModule } from './event/event.module';
 import { EventDetailsService } from './shared/event-details.service';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    BoxComponent,
-    NavBarComponent
-  ],
+  declarations: [AppComponent, BoxComponent, NavBarComponent],
   imports: [
-
     BrowserModule,
     SharedModule,
     AboutModule,
     EventModule,
     ContactUsModule,
     EventDetailsModule,
-    SermonModule
+    SermonModule,
   ],
   providers: [EventDetailsService, SermonService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
-
---
+++ src/app/shared/constants/routes.ts
@@ -0,0 +1,5 @@
+export const Home = 'home';
+export const Blog = 'blog';
+export const Sermon = 'sermon';
+export const event = 'event';
+export const gallery = 'gallery';
